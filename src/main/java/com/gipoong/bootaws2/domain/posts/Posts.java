2e5da34d730f39db227439d0cb4b73da9960065a
Gipoong <gipoong@naver.com>
2022-08-14 23:25:22 +0900

    공통 createDate, modifiedDate 추가

--- src/main/java/com/gipoong/bootaws2/domain/posts/Posts.java
+++ src/main/java/com/gipoong/bootaws2/domain/posts/Posts.java
@@ -1,16 +1,21 @@
 package com.gipoong.bootaws2.domain.posts;
 
+import com.gipoong.bootaws2.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
 
 @NoArgsConstructor
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
