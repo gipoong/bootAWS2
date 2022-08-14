3a0a2153df531627aedebe2957e134efd54c1a36
Gipoong <gipoong@naver.com>
2022-08-14 20:00:25 +0900

    Initial commit

---
+++ src/main/java/com/gipoong/bootaws2/BootAws2Application.java
@@ -0,0 +1,14 @@
+package com.gipoong.bootaws2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAws2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BootAws2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gipoong/bootaws2/BootAws2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gipoong.bootaws2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAws2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
